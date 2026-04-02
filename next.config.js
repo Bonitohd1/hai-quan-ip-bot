72123ab61518b6dc66e12686f3d4775d4bee1dc1
Your Name <you@example.com>
2026-04-03 03:04:36 +0700

    chore: Vercel Bundle Optimization (Exclude SWC and Typescript) to fix 250MB limit

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@swc/wasm',
+        'node_modules/typescript',
+      ],
+    },
+  },
   serverExternalPackages: ['@prisma/client', 'pdf-lib'],
 };
 
